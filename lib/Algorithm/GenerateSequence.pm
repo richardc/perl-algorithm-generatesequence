83a50505c952b915191d03f6d5f8ea4c7eadbc71
richardc <richardc@c56e198b-7db7-0310-b3ab-8e406f684e33>
2003-09-06 04:26:54 +0000

    make it work then
    git-svn-id: file:///web/svn.unixbeard.net/repo/richardc/perl/Algorithm-GenerateSequence@1545 c56e198b-7db7-0310-b3ab-8e406f684e33

--- lib/Algorithm/GenerateSequence.pm
+++ lib/Algorithm/GenerateSequence.pm
@@ -1,21 +1,78 @@
 use strict;
+use diagnostics;
 package Algorithm::GenerateSequence;
 our $VERSION = '1.21';
 use base 'Class::Accessor::Fast';
+__PACKAGE__->mk_accessors(qw( _values _counters _started _ended ));
+
+=head1 NAME
+
+Algorithm::GenerateSequence - a sequence generator
+
+=head1 SYNOPSIS
+
+=head1 METHODS
+
+=head2 new
+
+=cut
 
 sub new {
     my $class = shift;
-    $class->new({ values => [ @_ ], counters => [ map { 0 } @_ ] });
+    $class->SUPER::new({
+        _values   => [ @_ ],
+        _counters => [ map { 0 } @_ ],
+    });
+}
+
+sub reset {
+    my $self = shift;
+    $_ = 0 for @{ $self->_counters };
+    $self->_started(0);
+    $self->_ended(0);
 }
 
 sub next {
     my $self = shift;
 
-    # mmm, long addition
+    return if $self->_ended;
 
+    if ($self->_started) {
+        my $max = @{ $self->_counters } - 1;
+
+        # mmm, long addition
+        do {
+            my $new = ++$self->_counters->[ $max ];
+            # check for overflow
+            goto DONE if $new % @{ $self->_values->[ $max ] };
+            $self->_counters->[ $max ] = 0;
+        } while --$max >= 0;
+      DONE:
+        if ($max < 0) {
+            $self->_ended(1);
+            return;
+        }
+    }
+    $self->_started(1);
 
     my $i = 0;
-    return [ map {
-        $self->values->[ $i++ ][ $_ ] ]
-    } @{ $self->counters } ];
+    return map {
+        $self->_values->[ $i++ ][ $_ ]
+    } @{ $self->_counters };
 }
+
+sub as_list {
+    my $self = shift;
+    $self->reset;
+    my @results;
+
+    while (my @next = $self->next) {
+        push @results, \@next;
+    }
+
+    $self->reset;
+    return @results;
+}
+
+1;
+__END__
--- t/ags.t
+++ t/ags.t
@@ -1,6 +1,6 @@
 #!perl -w
 use strict;
-use Test::More tests => 10;
+use Test::More tests => 17;
 
 my $class = 'Algorithm::GenerateSequence';
 require_ok( $class );
@@ -8,12 +8,20 @@ my $iter = $class->new(
     [qw( alpha beta )], [qw( one two )], [qw( one two three )] );
 
 isa_ok( $iter, $class );
-is_deeply( $class->next, [qw( alpha one one )] );
-is_deeply( $class->next, [qw( alpha one two )] );
-is_deeply( $class->next, [qw( alpha one three )] );
-is_deeply( $class->next, [qw( alpha two one )] );
-is_deeply( $class->next, [qw( alpha two two )] );
-is_deeply( $class->next, [qw( alpha two three )] );
-is_deeply( $class->next, [qw( beta one one )] );
-is_deeply( $class->next, [qw( beta one two )] );
-is_deeply( $class->next, [qw( beta one three )] );
+is_deeply( [ $iter->next ], [qw( alpha one one )] );
+is_deeply( [ $iter->next ], [qw( alpha one two )] );
+is_deeply( [ $iter->next ], [qw( alpha one three )] );
+is_deeply( [ $iter->next ], [qw( alpha two one )] );
+is_deeply( [ $iter->next ], [qw( alpha two two )] );
+is_deeply( [ $iter->next ], [qw( alpha two three )] );
+is_deeply( [ $iter->next ], [qw( beta  one one )] );
+is_deeply( [ $iter->next ], [qw( beta  one two )] );
+is_deeply( [ $iter->next ], [qw( beta  one three )] );
+is_deeply( [ $iter->next ], [qw( beta  two one )] );
+is_deeply( [ $iter->next ], [qw( beta  two two )] );
+is_deeply( [ $iter->next ], [qw( beta  two three )] );
+ok( !$iter->next, "end" );
+ok( !$iter->next, "still the end" );
+
+$iter->reset;
+is_deeply( [ $iter->next ], [qw( alpha one one )], "reset the iterator" );
